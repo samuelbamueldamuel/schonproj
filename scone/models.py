b0f85fdad169deea2a6fe51b8e5174b678bc20fc
samuelbamueldamuel <samdinardi@gmail.com>
2023-09-16 21:51:02 -0400

    able to save to DB

--- scone/models.py
+++ scone/models.py
@@ -3,17 +3,13 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(null=False),
     symbol = models.CharField(max_length=5, primary_key=5),
+    ESG = models.IntegerField(null=True)
+    EV = models.IntegerField(null=True)
+    NH = models.IntegerField(null=True)
+    CT = models.IntegerField(null=True)
+    FS = models.IntegerField(null=True)
 
-class Sustainability(models.Model):
-    symbol = models.OneToOneField(Company, related_name='susSymbol', on_delete=models.CASCADE)
-    ESG = models.IntegerField()
-    carbon = models.IntegerField()
 
-class Stock(models.Model):
-    symbol = models.ForeignKey(Company, related_name='stockSymbol', on_delete=models.CASCADE)
-    date = models.CharField(max_length=10)
-    price = models.IntegerField()
-    volume = models.IntegerField()
 
 
 
--- scone/views.py
+++ scone/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from scone.scripts.scraper import scrape
 from django.shortcuts import render
 from django.http import HttpResponse
+from scone.models import Company
+from scone.scripts.randomMetrics import alg
 
 def fakeurl(request):
 
@@ -13,7 +15,20 @@ def work(request):
     if request.method == 'POST':
     # Process the form data
         name = request.POST.get('companySearch')
-        two, three = scrape(name)
+        two, three, env, soc, ticket = scrape(name)
+
+        FS, EV, NH, TR = alg(env, soc, three)
+
+
+        company = Company(name=name, symbol=ticket, FS=FS, EV=EV, TR=TR )
+        company.save()
+
+
+        
+
+
+
+
 
         context = {
             'two': two,
