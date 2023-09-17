64348f09c1c3932ec97390a53fc90d89e0a400a3
Les <trevorstsurin03@gmail.com>
2023-09-16 22:20:45 -0400

    5 data points

--- scone/scripts/scraper.py
+++ scone/scripts/scraper.py
@@ -20,19 +20,40 @@ def scrape(name):
     time.sleep(1)
     search_bar.send_keys(Keys.ENTER)
     time.sleep(1)
-
+    ticket = driver.find_element(By.ID, "company-ticker")
+    time.sleep(1)
     element2022 = driver.find_element(By.CLASS_NAME, "scoreModule__score")
 
     driver.execute_script("arguments[0].scrollIntoView();", element2022)
 
     element2021 = driver.find_element(By.CLASS_NAME, "scoreModule__subtitle")
 
+    environmental = driver.find_element(By.XPATH, """/html/body/div[3]/div[10]/div[1]/div[2]/div[2]/div/figure/div[1]/div[2]/ul/li[1]""")
+    environmental = environmental.text.split(".")[1]
+
+    social = driver.find_element(By.XPATH,
+                                        """/html/body/div[3]/div[10]/div[1]/div[2]/div[2]/div/figure/div[2]/div[2]/ul/li[1]""")
+    social = social.text.split(".")[1]
+
+
+
     data2022 = element2022.text
-    minus = element2021.text.split()[1]
+    minus = element2021.text.split()[2]
+    minus = int(minus)
+    ticket = ticket.text.split()[1]
 
-    data2021 = int(data2022) + abs(int(minus))
 
-    return data2021, int(data2022)
+
+
+    if minus < 0:
+        data2021 = int(data2022) + abs((minus))
+    else:
+        data2021 = int(data2022) - abs((minus))
+
+
+
+    return data2021, int(data2022), ticket, environmental, social,ticket
+
 
 
 
